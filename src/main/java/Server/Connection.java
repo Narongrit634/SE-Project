a0d11ed508d4bd6efd8406c657f27d8baade5570
Teerayuth Artidakorn <bomzakup_123@hotmail.com>
2019-09-10 13:07:52 +0700

    Connect to Online Database

--- src/main/java/Server/Connection.java
+++ src/main/java/Server/Connection.java
@@ -3,16 +3,20 @@ package Server;
 import java.sql.*;
 
 public class Connection {
-    private static String driver = "org.sqlite.JDBC";
-    private static String dbURL = "jdbc:sqlite:sedatabase.db";
+//    private static String driver = "org.sqlite.JDBC";
+//    private static String dbURL = "jdbc:sqlite:sedatabase.db";
+    private  static String driver = "com.mysql.jdbc.Driver";
+    private static String dbURL ="jdbc:mysql://remotemysql.com:3306/ytzEjdpCNf";
+    private static String dbUser = "ytzEjdpCNf";
+    private static String dbPass = "L9LcYDD81I";
     public static void check(){
         try {
             // setup
             Class.forName(driver);
-            java.sql.Connection conn = DriverManager.getConnection(dbURL);
+            java.sql.Connection conn = DriverManager.getConnection(dbURL,dbUser,dbPass);
             if (conn != null) {
                 System.out.println("Connected to the database....");
-                String query = "Select * from Users";
+                String query = "Select * from users";
                 Statement statement = conn.createStatement();
                 ResultSet resultSet = statement.executeQuery(query);
                 while (resultSet.next()) {
@@ -20,7 +24,7 @@ public class Connection {
                     String name = resultSet.getString(2);
                     String email = resultSet.getString("email");
 
-                    System.out.println("id:"+id+" name:"+name+" price: "+email);
+                    System.out.println("id:"+id+" name:"+name+" email: "+email);
             }
                 // close connection
                 conn.close();
